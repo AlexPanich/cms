501d5fd94a19ca1c996a2ce7a51ac0af7c9a9e72
AlexPanich <nasada@yandex.ru>
2016-07-15 17:48:00 +0300

    role

--- resources/views/dashboard/role/edit.blade.php
+++ resources/views/dashboard/role/edit.blade.php
@@ -1,52 +1,44 @@
 @extends('layouts.dashboard')
 
-@inject('role', 'App\Role')
+@inject('permission', 'App\Permission')
 
 @section('content')
     @include('errors.list')
-    <form class="" action="{{ route('update_user', $user->id) }}" method="post">
+    <form class="" action="{{ route('update_role', $role->id) }}" method="post">
         {{ csrf_field() }}
         <div class="control-group">
-            <label class="control-label" for="name">Логин</label>
+            <label class="control-label" for="name">Название</label>
             <div class="controls">
                 <div class="input-prepend">
-                    <input class="form-control" type="text" name="name" id="name" value="{{ old('name', $user->name) }}">
+                    <input class="form-control" type="text" name="name" id="name" value="{{ old('name', $role->name) }}">
                 </div>
             </div>
         </div>
         <div class="control-group">
-            <label class="control-label" for="email">E-mail</label>
+            <label class="control-label" for="label">Описание</label>
             <div class="controls">
                 <div class="input-prepend">
-                    <input class="form-control" type="email" name="email" id="email" value="{{ old('email', $user->email) }}">
+                    <input class="form-control" type="text" name="label" id="label" value="{{ old('label', $role->label) }}">
                 </div>
             </div>
         </div>
         <div class="control-group">
-            <label class="control-label" for="password">Пароль</label>
+            <label class="control-label" for="permissions">Привелегии</label>
             <div class="controls">
                 <div class="input-prepend">
-                    <input class="form-control" type="text" name="password" id="password" value="{{ old('password') }}">
-                </div>
-            </div>
-        </div>
-        <div class="control-group">
-            <label class="control-label" for="roles">Роли</label>
-            <div class="controls">
-                <div class="input-prepend">
-                    <select name="roles[]" id="roles" multiple>
-                        @foreach($role::all() as $role)
-                            <option value="{{ $role->id }}"
-                                @if(in_array($role->id, old('roles', $user->roles->keyBy('id')->keys()->all())))
+                    <select name="permissions[]" id="permissions" multiple>
+                        @foreach($permission::all() as $permission)
+                            <option value="{{ $permission->id }}"
+                                    @if(in_array($permission->id, old('permission', [])))
                                     selected
-                                @endif
-                            >{{ $role->label }}</option>
+                                    @endif
+                            >{{ $permission->label }}</option>
                         @endforeach
                     </select>
                 </div>
             </div>
         </div>
         <br>
-        <input type="submit" value="Отредактировать пользователя" class="btn btn-danger">
+        <input type="submit" value="Создать роль" class="btn btn-danger">
     </form>
 @endsection
